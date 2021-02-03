3c8434025e6d03d4a157c8dd9fc7fc0ffaa01b6f
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2021-02-03 12:59:08 +0200

    test

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ require('./config/mongooseConfig')(app);
 app.listen(config.PORT, () => console.log(`Server is listening on ${config.PORT}`));
 
 
+app.get('/', (req, res) => {
+    res.send('Любов моя това е началото на новият ти сайт. Обичам те много')
+})
