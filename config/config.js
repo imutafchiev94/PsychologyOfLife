5cba3fb35274177dfe4c8bb8856550e494ecab66
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2021-02-03 13:46:23 +0200

    t

--- config/config.js
+++ config/config.js
@@ -1,12 +1,6 @@
 configuration = {
-    development: {
-        PORT: 5000,
-        connectionString: "mongodb+srv://admin:admin@psychologyoflife.cwidl.mongodb.net/psychologyOfLife?retryWrites=true&w=majority" 
-    },
-    production: {
-        PORT: 80,
-        connectionString: "mongodb+srv://admin:admin@psychologyoflife.cwidl.mongodb.net/psychologyOfLife?retryWrites=true&w=majority" 
-    }
+    PORT: 5000,
+    connectionString: "mongodb+srv://admin:admin@psychologyoflife.cwidl.mongodb.net/psychologyOfLife?retryWrites=true&w=majority" 
 }
 
-module.exports = configuration[process.env.NODE_ENV.trim()];
+module.exports = configuration;
