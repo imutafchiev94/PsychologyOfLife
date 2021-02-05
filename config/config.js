6e7fe422a83a8678c406a0b5bcc9894f03226660
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2021-02-05 14:20:57 +0200

    change config file

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 configuration = {
     PORT: process.env.PORT || 3000,
-    connectionString: "mongodb+srv://admin:admin@psychologyoflife.cwidl.mongodb.net/psychologyOfLife?retryWrites=true&w=majority" 
+     
 }
 
 // cloudinary secret: uiYiEPH5drPsgdwvHXdvLtBcTGU
-// cloud name: dlv09omf2
+// cloud name: PsychologyOfLife
 // Api Key: 252246829665622
 
 module.exports = configuration;
